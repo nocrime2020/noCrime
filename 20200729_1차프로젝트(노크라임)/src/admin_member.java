e8c6973b7ce65918eb436bcdc37804e7665d0282
SMT053 <mha31000@gmail.com>
2020-07-30 17:35:32 +0900

    관리자 화면 디자인 수정

--- 20200729_1차프로젝트(노크라임)/src/admin_member.java
+++ 20200729_1차프로젝트(노크라임)/src/admin_member.java
@@ -1,5 +1,6 @@
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -7,10 +8,13 @@ import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.Color;
+import javax.swing.JTable;
+import javax.swing.JButton;
 
 public class admin_member {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -46,7 +50,9 @@ public class admin_member {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lbl_title = new JLabel("\uB178(Know) \uD06C\uB77C\uC784");
+		
+		JLabel lbl_title = new JLabel("");
+		lbl_title.setIcon(new ImageIcon("C:\\Users\\SMT053\\Desktop\\\uAE30\uBCF8\uD504\uB85C\uC81D\uD2B8\uC0B0\uCD9C\uBB3C\\\uB178\uD06C\uB77C\uC784\\icon\\\uC791\uC740 \uD22C\uBA85.png"));
 		lbl_title.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -58,26 +64,38 @@ public class admin_member {
 			}
 		});
 		lbl_title.setFont(new Font("±º∏≤", Font.PLAIN, 23));
-		lbl_title.setBounds(12, 10, 194, 27);
+		lbl_title.setBounds(12, 10, 103, 56);
 		frame.getContentPane().add(lbl_title);
 		
-		JLabel lblNewLabel_2_1 = new JLabel("\uB85C\uADF8\uC544\uC6C3");
+		JLabel lblNewLabel_2_1 = new JLabel("");
+		lblNewLabel_2_1.setIcon(new ImageIcon("C:\\Users\\SMT053\\Desktop\\\uAE30\uBCF8\uD504\uB85C\uC81D\uD2B8\uC0B0\uCD9C\uBB3C\\\uB178\uD06C\uB77C\uC784\\icon\\off_logout_17916.png"));
 		lblNewLabel_2_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-			
+				
 				frame.dispose();
 //				login.main(null);
 				login login = new login();
+				
 			}
 		});
-		lblNewLabel_2_1.setBounds(278, 10, 81, 15);
+		lblNewLabel_2_1.setBounds(307, 10, 40, 29);
 		frame.getContentPane().add(lblNewLabel_2_1);
 		
 		JLabel lblNewLabel_3 = new JLabel("\uD68C\uC6D0 \uAD00\uB9AC");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_3.setFont(new Font("«‘√ ∑“µ∏øÚ", Font.BOLD, 24));
-		lblNewLabel_3.setBounds(50, 59, 180, 60);
+		lblNewLabel_3.setBounds(90, 76, 180, 60);
 		frame.getContentPane().add(lblNewLabel_3);
+		
+		table = new JTable();
+		table.setBounds(37, 146, 287, 493);
+		frame.getContentPane().add(table);
+		
+		JButton btnNewButton = new JButton("\uAC15\uC81C \uD0C8\uD1F4");
+		btnNewButton.setFont(new Font("«‘√ ∑“µ∏øÚ", Font.BOLD, 14));
+		btnNewButton.setBounds(37, 666, 287, 43);
+		frame.getContentPane().add(btnNewButton);
 	}
 
 }
