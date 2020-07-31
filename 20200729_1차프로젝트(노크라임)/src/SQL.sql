ce022a23afc1c37d88f65564ac681cb2ed52bcd8
SMT053 <mha31000@gmail.com>
2020-07-31 18:26:37 +0900

    ㅁㅁ

--- 20200729_1차프로젝트(노크라임)/src/SQL.sql
+++ 20200729_1차프로젝트(노크라임)/src/SQL.sql
@@ -8,7 +8,76 @@ EMAIL varchar2(40) not null,
 constraints member_id_pk primary key(ID),
 constraints member_email_uk unique (email)
 );
-select * from member
+
+create table tip_info
+(tip_info_id varchar2(20),
+cr_type_id varchar2(20) not null,
+cr_date date default SYSDATE not null,
+cr_loc_id varchar2(20) not null,
+evidence varchar2(70) not null,
+cr_name varchar2(70),
+constraints tipinfo_tipinfoid_pk primary key(tip_info_id),
+constraint tipinfo_crtypeid_fk foreign key(cr_type_id) references crime_type(cr_type_id),
+constraint tipinfo_crlocid_fk foreign key(cr_loc_id) references crime_location(cr_loc_id)
+);
+
+CREATE SEQUENCE tip_info_id_seq
+increment by 1
+start with 1
+nocache;
+
+create table crime
+(cr_id varchar2(20) not null,
+cr_loc_id varchar2(20) not null,
+cr_date date not null,
+cr_type_id varchar2(20) not null,
+tip_id varchar2(20),
+cr_name varchar2(70),
+constraints crime_crid_uk unique (cr_id),
+constraints crime_cridld_pk primary key(cr_id, cr_loc_id, cr_date),
+constraint crime_crlocid_fk foreign key(cr_loc_id) references crime_location(cr_loc_id),
+constraint crime_crtypeid_fk foreign key(cr_type_id) references crime_type(cr_type_id),
+constraint crime_tipid_fk foreign key(tip_id) references tipoff(tip_id)
+);
+
+CREATE SEQUENCE cr_id_seq
+increment by 1
+start with 1
+nocache;
+
+create table crime_type
+(cr_type_id varchar2(20),
+cr_type varchar2(20) not null,
+constraints crimetype_crtypeid_pk primary key(cr_type_id),
+constraints crimetype_crtype_CK CHECK (cr_type IN('살인', '상해폭행', '강간추행', '절도강도', '납치', '약취유인'))
+);
+
+CREATE SEQUENCE cr_type_id_seq
+increment by 1
+start with 1
+nocache;
+
+create table crime_location
+(cr_loc_id varchar2(20),
+cr_city varchar2(20) not null,
+cr_gu varchar2(20),
+cr_address varchar2(70) not null,
+constraints crimelocation_crlocid_pk primary key(cr_loc_id)
+);
+
+CREATE SEQUENCE cr_loc_id_seq
+increment by 1
+start with 1
+nocache;
+
+create table alarm
+(id varchar2(20),
+set_start number(20),
+set_end number(20),
+cr_cnt number(20),
+constraints alarm_id_pk primary key(id),
+constraint alarm_id_fk foreign key(id) references member(id)
+);
 
 
 insert into member values('admin','admin','마현아','dkskdfjk@f.com');
@@ -45,19 +114,15 @@ insert into crime_type values('5','상해폭행');
 insert into crime_location values('118','광주광역시','남구','송암로58번길');
 insert into crime_location values('119','광주광역시','남구','송암로76번길');
 
-insert into tip_info values('','1000001','강간추행','2017-09-24','118',null);
-insert into tip_info values('','1000002','강간추행','2019-08-14','119',null);
-insert into tip_info values('','1000003','약취유인','2018-05-24','118',null);
-insert into tip_info values('','1000004','살인','2018-11-05','119',null);
-insert into tip_info values('','1000005','상해폭행','2018-12-06','118',null);
-insert into tip_info values('','1000006','상해폭행','2020-05-08','119',null);
-insert into tip_info values('','1000007','약취유인','2019-09-04','118',null);
-insert into tip_info values('','1000008','절도강도','2020-05-29','118',null);
-insert into tip_info values('','1000009','약취유인','2020-05-28','118',null);
-
-insert into tipoff values('1000006','상해폭행','2020-05-08','119',null);
-insert into tipoff values('1000008','절도강도','2020-05-29','118',null);
-insert into tipoff values('1000009','약취유인','2020-05-28','118',null);
+insert into tip_info values('1000001','강간추행','2017-09-24','118',null,null);
+insert into tip_info values('1000002','강간추행','2019-08-14','119',null,null);
+insert into tip_info values('1000003','약취유인','2018-05-24','118',null,null);
+insert into tip_info values('1000004','살인','2018-11-05','119',null,null);
+insert into tip_info values('1000005','상해폭행','2018-12-06','118',null,null);
+insert into tip_info values('1000006','상해폭행','2020-05-08','119',null,null);
+insert into tip_info values('1000007','약취유인','2019-09-04','118',null,null);
+insert into tip_info values('1000008','절도강도','2020-05-29','118',null,null);
+insert into tip_info values('1000009','약취유인','2020-05-28','118',null,null);
 
 insert into alarm values('abd12','7','13','5');
 insert into alarm values('rksk12','7','15','10');
