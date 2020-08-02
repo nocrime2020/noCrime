37a2173e1f4ea0b1085912620166da9d6af9054c
SMT053 <mha31000@gmail.com>
2020-08-02 14:58:20 +0900

    푸시 전

--- 20200729_1차프로젝트(노크라임)/src/myPage.java
+++ 20200729_1차프로젝트(노크라임)/src/myPage.java
@@ -136,7 +136,7 @@ public class myPage {
 		VO_alarm al_vo = dao.check_alarm2(vo);
 			if (cnt2 > 0) { // 아이디가 이미 등록되어 있을 때, 초기에 기존 알람시작시간 불러옴
 				//String combo_startalarm1 = combo_startalarm.setSelectedIndex(vo);	
-				combo_startalarm.setModel(new DefaultComboBoxModel(new String[] {al_vo.getSet_start() , "12:00 AM", "1:00 AM", "2:00 AM",
+				combo_startalarm.setModel(new DefaultComboBoxModel(new String[] {al_vo.getSet_start()+":00" , "12:00 AM", "1:00 AM", "2:00 AM",
 						"3:00 AM", "4:00 AM", "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM",
 						"12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM",
 						"9:00 PM", "10:00 PM", "11:00 PM" }));
@@ -157,7 +157,7 @@ public class myPage {
 		cnt2 = dao.check_alarm(vo);
 		VO_alarm al_vo1 = dao.check_alarm2(vo);
 		if (cnt2 > 0) { // 아이디가 이미 등록되어 있을 때, 초기에 기존 알람시작시간 불러옴
-			combo_endalarm.setModel(new DefaultComboBoxModel(new String[] {al_vo1.getSet_end() , "12:00 AM", "1:00 AM", "2:00 AM",
+			combo_endalarm.setModel(new DefaultComboBoxModel(new String[] {al_vo1.getSet_end()+":00" , "12:00 AM", "1:00 AM", "2:00 AM",
 					"3:00 AM", "4:00 AM", "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM", "11:00 AM",
 					"12:00 PM", "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM",
 					"9:00 PM", "10:00 PM", "11:00 PM" }));
